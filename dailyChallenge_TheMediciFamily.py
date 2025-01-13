77d6bf195f2b51e0858369f15b28a4bd1c600e77
PADMA PRIYA MURALI <74344564+PriyaMurali16@users.noreply.github.com>
2025-01-12 18:46:15 -0600

    Update dailyChallenge_TheMediciFamily.py

--- dailyChallenge_TheMediciFamily.py
+++ dailyChallenge_TheMediciFamily.py
@@ -1,1 +1,26 @@
+def medici_family(relation_name, relation_role):
+    if relation_name.lower() == "giovanni di bicci de' medici":
+        if relation_role.lower() == 'founder':
+            final_result = True
+        else :
+            final_result = False 
+    elif relation_name.lower() == "lorenzo de' medici":
+        if relation_role.lower() == 'patron':
+            final_result = True
+        else :
+            final_result = False 
+
+    elif relation_name.lower() == "cosimo de' medici":
+        if relation_role.lower() == 'politician':
+            final_result = True
+        else :
+            final_result = False 
+    
+    elif relation_name.lower() == "piero di cosimo de' medici":
+        if relation_role.lower() == 'father':
+            final_result = True
+        else :
+            final_result = False 
+    
+    return final_result
 
